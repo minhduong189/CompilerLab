725cfde28e38ed607384e410070eff9ea78b6ae9
minhduong189 <147677091+minhduong189@users.noreply.github.com>
2026-06-19 23:02:00 +0700

    commit

---
+++ src/codegen.c
@@ -0,0 +1,250 @@
+/* 
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#include <stdio.h>
+#include "reader.h"
+#include "codegen.h"  
+
+#define CODE_SIZE 10000
+extern SymTab* symtab;
+
+extern Object* readiFunction;
+extern Object* readcFunction;
+extern Object* writeiProcedure;
+extern Object* writecProcedure;
+extern Object* writelnProcedure;
+
+CodeBlock* codeBlock;
+
+// walk up the scope chain from the current scope to find how many
+// levels (nested blocks) away the variable/parameter was declared,
+// and resolve its local offset
+static void getVariableLevelOffset(Object* var, int* level, int* offset) {
+  Scope* scope;
+  Scope* s;
+
+  if (var->kind == OBJ_PARAMETER) {
+    scope = PARAMETER_SCOPE(var);
+    *offset = PARAMETER_OFFSET(var);
+  } else {
+    scope = VARIABLE_SCOPE(var);
+    *offset = VARIABLE_OFFSET(var);
+  }
+
+  *level = 0;
+  s = symtab->currentScope;
+  while (s != NULL && s != scope) {
+    s = s->outer;
+    (*level)++;
+  }
+}
+
+void genVariableAddress(Object* var) {
+  int level, offset;
+  getVariableLevelOffset(var, &level, &offset);
+
+  if (var->kind == OBJ_PARAMETER && var->paramAttrs->kind == PARAM_REFERENCE)
+    genLV(level, offset);  // reference param: the slot holds the address of the real variable
+  else
+    genLA(level, offset);
+}
+
+void genVariableValue(Object* var) {
+  int level, offset;
+
+  if (var->kind == OBJ_PARAMETER && var->paramAttrs->kind == PARAM_REFERENCE) {
+    // slot holds the address of the real variable: load that address, then dereference
+    genVariableAddress(var);
+    genLI();
+  } else {
+    // plain variable/value-parameter: load its value directly, no address needed
+    getVariableLevelOffset(var, &level, &offset);
+    genLV(level, offset);
+  }
+}
+
+int isPredefinedFunction(Object* func) {
+  return ((func == readiFunction) || (func == readcFunction));
+}
+
+int isPredefinedProcedure(Object* proc) {
+  return ((proc == writeiProcedure) || (proc == writecProcedure) || (proc == writelnProcedure));
+}
+
+void genPredefinedProcedureCall(Object* proc) {
+  if (proc == writeiProcedure)
+    genWRI();
+  else if (proc == writecProcedure)
+    genWRC();
+  else if (proc == writelnProcedure)
+    genWLN();
+}
+
+void genPredefinedFunctionCall(Object* func) {
+  if (func == readiFunction)
+    genRI();
+  else if (func == readcFunction)
+    genRC();
+}
+
+void genLA(int level, int offset) {
+  emitLA(codeBlock, level, offset);
+}
+
+void genLV(int level, int offset) {
+  emitLV(codeBlock, level, offset);
+}
+
+void genLC(WORD constant) {
+  emitLC(codeBlock, constant);
+}
+
+void genLI(void) {
+  emitLI(codeBlock);
+}
+
+void genINT(int delta) {
+  emitINT(codeBlock,delta);
+}
+
+void genDCT(int delta) {
+  emitDCT(codeBlock,delta);
+}
+
+Instruction* genJ(CodeAddress label) {
+  Instruction* inst = codeBlock->code + codeBlock->codeSize;
+  emitJ(codeBlock,label);
+  return inst;
+}
+
+Instruction* genFJ(CodeAddress label) {
+  Instruction* inst = codeBlock->code + codeBlock->codeSize;
+  emitFJ(codeBlock, label);
+  return inst;
+}
+
+void genHL(void) {
+  emitHL(codeBlock);
+}
+
+void genST(void) {
+  emitST(codeBlock);
+}
+
+void genCALL(int level, CodeAddress label) {
+  emitCALL(codeBlock, level, label);
+}
+
+void genEP(void) {
+  emitEP(codeBlock);
+}
+
+void genEF(void) {
+  emitEF(codeBlock);
+}
+
+void genRC(void) {
+  emitRC(codeBlock);
+}
+
+void genRI(void) {
+  emitRI(codeBlock);
+}
+
+void genWRC(void) {
+  emitWRC(codeBlock);
+}
+
+void genWRI(void) {
+  emitWRI(codeBlock);
+}
+
+void genWLN(void) {
+  emitWLN(codeBlock);
+}
+
+void genAD(void) {
+  emitAD(codeBlock);
+}
+
+void genSB(void) {
+  emitSB(codeBlock);
+}
+
+void genML(void) {
+  emitML(codeBlock);
+}
+
+void genDV(void) {
+  emitDV(codeBlock);
+}
+
+void genNEG(void) {
+  emitNEG(codeBlock);
+}
+
+void genCV(void) {
+  emitCV(codeBlock);
+}
+
+void genEQ(void) {
+  emitEQ(codeBlock);
+}
+
+void genNE(void) {
+  emitNE(codeBlock);
+}
+
+void genGT(void) {
+  emitGT(codeBlock);
+}
+
+void genGE(void) {
+  emitGE(codeBlock);
+}
+
+void genLT(void) {
+  emitLT(codeBlock);
+}
+
+void genLE(void) {
+  emitLE(codeBlock);
+}
+
+void updateJ(Instruction* jmp, CodeAddress label) {
+  jmp->q = label;
+}
+
+void updateFJ(Instruction* jmp, CodeAddress label) {
+  jmp->q = label;
+}
+
+CodeAddress getCurrentCodeAddress(void) {
+  return codeBlock->codeSize;
+}
+
+
+void initCodeBuffer(void) {
+  codeBlock = createCodeBlock(CODE_SIZE);
+}
+
+void printCodeBuffer(void) {
+  printCodeBlock(codeBlock);
+}
+
+void cleanCodeBuffer(void) {
+  freeCodeBlock(codeBlock);
+}
+
+int serialize(char* fileName) {
+  FILE* f;
+
+  f = fopen(fileName, "wb");
+  if (f == NULL) return IO_ERROR;
+  saveCode(codeBlock, f);
+  fclose(f);
+  return IO_SUCCESS;
+}
